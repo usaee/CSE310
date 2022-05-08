a9dabe1cb2b0ce9ae8aa3914d4021f85efb4ea48
usaee <44759143+usaee@users.noreply.github.com>
2022-05-07 20:54:38 -0600

    Update CSE_301_Module_1.py

--- Module1/CSE_301_Module_1.py
+++ Module1/CSE_301_Module_1.py
@@ -4,8 +4,9 @@ import pandas as pd
 import statistics as stats
 
 
-# Part 1
-
+#########################################################################################
+# Finding Day Which Day Users Tend To Walk The Farthest.
+#########################################################################################
 fname = "Daily_Activity_2022_27_02.csv"
 data_set = pd.read_csv(fname)
 
@@ -26,20 +27,32 @@ for index, row in days_distance.iterrows():
     day = row['ActivityDay']
     dist = row['TotalDistance']
 
+    # If the distance walked on the current day is
+    # greater than the last greatest distance recorded,
+    # then the value of the last 'best_day' variable is
+    # replaced with the distance for the current day.
     if dist > most:
         most = dist
         best_day = day
 
+    # I am using Sunday to mark the end of each week.
+    # Therefore, each time we reach the day Sunday,
+    # The day with the greatest distance walked will be
+    # Added to the best_days list and the variables 'best_day'
+    # and 'most' will be reset so that the next week can be analyzed.
     if day == 'Sun':
         best_days.append(best_day)
         best_day = str()
         most = 0
 
-result = stats.mode(best_days)
-print("This person tends to do the most walking on ({})".format(full_days[result]))
+result = stats.mode(best_days) # Computes the mode of the list of days.
 
-# Part 2
+print("Users tend to walk the farthest on ({})".format(full_days[result]))
 
+
+#########################################################################################
+# Finding Day Which Day Users Tend To Perform The Most Vigorus Exercise.
+#########################################################################################
 days_distance = data_set[['ActivityDay', 'VeryActiveDistance']]
 
 most = 0
@@ -47,16 +60,26 @@ best_days = []
 best_day = str()
 for index, row in days_distance.iterrows():
     day = row['ActivityDay']
-    dist = row['VeryActiveDistance']
+    active_dist = row['VeryActiveDistance']
 
-    if dist > most:
-        most = dist
+    # If the amount of activity performed on the current day is
+    # greater than the last greatest amount of activity recorded,
+    # then the value of the last 'best_day' variable is
+    # replaced with the amount of activity for the current day.
+    if active_dist > most:
+        most = active_dist
         best_day = day
 
+    # I am using Sunday to mark the end of each week.
+    # Therefore, each time we reach the day Sunday,
+    # The day with the greatest amount of activity will be
+    # Added to the best_days list and the variables 'best_day'
+    # and 'most' will be reset so that the next week can be analyzed.
     if day == 'Sun':
         best_days.append(best_day)
         best_day = str()
         most = 0
 
 result = stats.mode(best_days)
-print("This person tends to be the most active on ({})".format(full_days[result]))
+print("Users tend to perform the most vigorus exercise on ({})".format(full_days[result]))
+
